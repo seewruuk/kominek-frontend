cd082769c33a7334c981a88b2f9a58c517efdf40
sewru <sewruczekk@icloud.com>
2024-01-21 15:29:43 +0100

    chat

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,24 +1,74 @@
+"use client"
 import PlaceholderIcon from "../../public/assets/placeholder-icon.svg"
 import Image from "next/image";
+import {useEffect, useRef, useState} from "react";
+
 export default function Page() {
+
+    const [messages, setMessages] = useState([]);
+    const endOfMessagesRef = useRef(null);
+    const [message, setMessage] = useState("");
+
+    const receiveMessage = (text) => {
+        const newMessage = {id: messages.length + 1, text, sender: 'other'};
+        setMessages([...messages, newMessage]);
+    };
+
+    useEffect(() => {
+        endOfMessagesRef.current?.scrollIntoView({behavior: 'smooth'});
+    }, [messages]);
+
+
+    const sendMessage = (text) => {
+        const newMessage = {id: messages.length + 1, text, sender: 'user'};
+        setMessages([...messages, newMessage]);
+    };
+
+
     return (
-        <div className={"relative"}>
+        <section className={"px-[15px]"}>
 
-            <div className={"h-screen pb-[160px] px-[15px] flex flex-col gap-4"}>
-
-                <div className={"flex-grow"}>Chat</div>
-                <form className={"flex justify-center items-center bg-[#292936] rounded-md p-[16px]"}>
-                    <input className={"bg-transparent flex-grow"} placeholder={"Napisz wiadomość..."}/>
-
-
-
-                    <div className={"px-5 bg-accentColor h-full aspect-square grid place-items-center rounded-lg" }>
-                        <Image src={PlaceholderIcon} alt={"placeholder"} width={15} height={15} className={""}/>
-                    </div>
+            <div className="chat-container">
+                <div className="messages-container">
+                    {messages.map((message) => (
+                        <div key={message.id} className={`message ${message.sender}`}>
+                            {message.text}
+                        </div>
+                    ))}
+                    <div ref={endOfMessagesRef}/>
+                </div>
+                <form className={"flex items-center rounded-md bg-[#292936] px-[16px] py-[12px]"}
+                      onKeyPress={(e) => {
+                          if (e.key === 'Enter') {
+                              e.preventDefault();
+                          }
+                        }}
+                      >
+                    <input
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        type="text"
+                        className={"flex-grow bg-transparent outline-none text-white"}
+                        placeholder="Napisz wiadomość..."
+                        onKeyPress={(e) => {
+                            if (e.key === 'Enter') {
+                                sendMessage(e.target.value);
+                                e.target.value = '';
+                            }
+                        }}
+                    />
+                    <span className={"p-3 bg-accentColor rounded-md"}
+                        onClick={() => {
+                            sendMessage(message);
+                            setMessage('');
+                        }}
+                    >
+                        <Image src={PlaceholderIcon} alt={"placeholder-icon"} width={12} height={12}/>
+                    </span>
                 </form>
 
             </div>
 
-        </div>
+        </section>
     )
 }
